423b38c4a18a9ac6b67dfe406ceb5f0bfe5e3964
emma-obelenge <obeleemmanuel@gmail.com>
2024-03-27 19:31:37 +0000

    fix betty errors for task 14

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,10 +25,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree != NULL)
+	{
+		size_t left_height = binary_tree_height(tree->left);
+		size_t right_height = binary_tree_height(tree->right);
+		return ((left_height > right_height ? left_height : right_height) + 1);
+	}
+	else
 		return (0);
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
 }
