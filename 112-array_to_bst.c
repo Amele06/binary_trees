e3a8cc330bb1ada4c238542623f0ae4082d4aaeb
emma-obelenge <obeleemmanuel@gmail.com>
2024-03-29 15:58:49 +0100

    add adv task files

---
+++ 112-array_to_bst.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - turns an array to a BST tree
+ * @array: array to turns to BST tree
+ * @size: size of array
+ * Return: BST tree from array
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *tree = NULL;
+	size_t i, j;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (bst_insert(&tree, array[i]) == NULL)
+				return (NULL);
+		}
+	}
+
+	return (tree);
+}
