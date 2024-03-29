8aea329b06876aa4283d382388e7533d48a4c510
emma-obelenge <obeleemmanuel@gmail.com>
2024-03-29 16:58:11 +0100

    update header file for adv110 task

--- binary_trees.h
+++ binary_trees.h
@@ -91,5 +91,7 @@ levelorder_queue_t *create_node(binary_tree_t *node);
 void free_queue(levelorder_queue_t *head);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int is_bst_helper(const binary_tree_t *tree, int bottom, int top);
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
