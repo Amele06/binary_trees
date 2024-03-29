76b396c5b9739b250a5691de843786c4ac3fb6be
emma-obelenge <obeleemmanuel@gmail.com>
2024-03-29 16:53:25 +0100

    update adv103 task compilation issues

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,27 +7,20 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *temp, *pivot_pnt;
+	binary_tree_t *pivot;
 
 	if (tree == NULL || tree->right == NULL)
-		return (NULL);
-
-	pivot_pnt = tree->right;
-	temp = pivot_pnt->left;
-	pivot_pnt->left = tree;
-	tree->right = temp;
-	if (temp != NULL)
-		temp->parent = tree;
-	temp = tree->parent;
-	tree->parent = pivot_pnt;
-	pivot_pnt->parent = temp;
-	if (temp != NULL)
 	{
-		if (temp->left == tree)
-			temp->left = pivot_pnt;
-		else
-			temp->right = pivot_pnt;
+		return (NULL);
 	}
-
-	return (pivot_pnt);
+	pivot = tree->right;
+	tree->right = pivot->left;
+	if (pivot->left != NULL)
+	{
+		pivot->left->parent = tree;
+	}
+	pivot->left = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
 }
